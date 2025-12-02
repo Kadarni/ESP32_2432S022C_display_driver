32f0f8f1c40c213b39542acb71bfbe187a9286f9
Kistva <kadarnemet.istvan@gmail.com>
2025-12-02 22:26:32 +0100

    fix display.py

--- esphome/components/custom_display/display.py
+++ esphome/components/custom_display/display.py
@@ -2,3 +2,6 @@ from esphome.components import display
 import esphome.config_validation as cv
 
 CONFIG_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend({}).extend(cv.COMPONENT_SCHEMA)
+
+def to_code(config):
+    return
